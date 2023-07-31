3a569918a6d3c20b30826810913575cfca8ef616
Mari Calmer <superlassig@gmail.com>
2023-07-31 19:06:38 +0200

    fix: add .png to img url in generator

--- app/models/services/image_url_generator.rb
+++ app/models/services/image_url_generator.rb
@@ -16,6 +16,6 @@ class Services::ImageUrlGenerator
 
   def create_image_url(slug)
     puts "variable: #{ENV['CLOUDINARY_CLOUD_NAME']}"
-    return "https://res.cloudinary.com/#{ENV['CLOUDINARY_CLOUD_NAME']}/image/upload/#{slug}"
+    return "https://res.cloudinary.com/#{ENV['CLOUDINARY_CLOUD_NAME']}/image/upload/#{slug}.png"
   end
 end
