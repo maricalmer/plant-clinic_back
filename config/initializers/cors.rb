438cdab6d3b3bc880b635139f910c67551e3a7b7
Mari Calmer <superlassig@gmail.com>
2023-08-01 22:38:50 +0200

    fix: add homepage + form page

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://mellifluous-arithmetic-b968bd.netlify.app/'
+    origins 'https://mellifluous-arithmetic-b968bd.netlify.app', 'https://mellifluous-arithmetic-b968bd.netlify.app/create'
 
     resource '*',
       headers: :any,
