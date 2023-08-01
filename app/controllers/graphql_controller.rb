52b2923a25673c03f778f84d67b325d4ff11e517
Mari Calmer <superlassig@gmail.com>
2023-08-01 17:36:42 +0200

    fix: forgery mixed strategy in graphql controller

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -3,6 +3,7 @@ class GraphqlController < ApplicationController
   # If accessing from outside this domain, nullify the session
   # This allows for outside API access while preventing CSRF attacks,
   # but you'll have to authenticate your user separately
+  skip_before_action :protect_from_forgery
   protect_from_forgery with: :null_session
 
   def execute
