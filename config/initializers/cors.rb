a679a57885d6b572f61c3bea3fcd78802fc5aad9
Mari Calmer <superlassig@gmail.com>
2023-08-02 01:04:40 +0200

    fix: add root to resources for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://mellifluous-arithmetic-b968bd.netlify.app'
 
-    resource '*',
+    resource '/',
       headers: 'x-domain-token',
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
