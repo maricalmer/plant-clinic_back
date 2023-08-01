02c9f48129cc63d229456dda7b267ddf870b81f9
Mari Calmer <superlassig@gmail.com>
2023-08-02 01:00:10 +0200

    fix: change headers option in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,8 +2,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://mellifluous-arithmetic-b968bd.netlify.app'
 
-    resource '/',
-      headers: :any,
+    resource '*',
+      headers: 'x-domain-token',
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
