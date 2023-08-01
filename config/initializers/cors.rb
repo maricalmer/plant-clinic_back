2cbf9aa7433eba06bf14c5c9fab948cb8646c572
Mari Calmer <superlassig@gmail.com>
2023-08-01 23:30:08 +0200

    fix: remove action controller forgery protection false in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,4 +9,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 end
 
-Rails.application.config.action_controller.forgery_protection_origin_check = false
+# Rails.application.config.action_controller.forgery_protection_origin_check = false
