ff874d19baac92342d461f3ec34782f048571fbf
Mari Calmer <superlassig@gmail.com>
2023-08-01 19:50:15 +0200

    fix: forgery null_session in graphql controller

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -3,7 +3,6 @@ class GraphqlController < ApplicationController
   # If accessing from outside this domain, nullify the session
   # This allows for outside API access while preventing CSRF attacks,
   # but you'll have to authenticate your user separately
-  skip_before_action :protect_from_forgery
   protect_from_forgery with: :null_session
 
   def execute
