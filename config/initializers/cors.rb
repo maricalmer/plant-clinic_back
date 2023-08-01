0c0e8184601c93512e43d016c5896198b13745fd
Mari Calmer <superlassig@gmail.com>
2023-08-01 23:11:34 +0200

    fix: open origins for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://mellifluous-arithmetic-b968bd.netlify.app:80'
+    origins '*'
 
     resource '*',
       headers: :any,
