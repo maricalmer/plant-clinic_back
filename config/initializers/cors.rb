68ba1c2e688c0c169bc406e92fea3092d60ea87c
Mari Calmer <superlassig@gmail.com>
2023-08-02 15:42:36 +0200

    fix: remove cors initializer

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,1 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'https://mellifluous-arithmetic-b968bd.netlify.app'
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: false
-  end
-end
-
-Rails.application.config.action_controller.forgery_protection_origin_check = false
