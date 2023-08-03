d01c0e82c49271889102f0a6c2dae44b63c771d5
Mari Calmer <superlassig@gmail.com>
2023-08-03 14:34:45 +0200

    fix: add cloudinary to cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://mellifluous-arithmetic-b968bd.netlify.app'
+    origins 'https://mellifluous-arithmetic-b968bd.netlify.app', 'https://api.cloudinary.com'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
